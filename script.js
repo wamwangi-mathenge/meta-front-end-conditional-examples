5c0d96d025689f2ecf39e576422b125d3cec46c1
Brian <wamwangimathenge@gmail.com>
2023-07-20 12:21:14 +0300

    Output traffic signals to drivers
    This commit demonstrates how to write a traffic signage based on
    traffic lights using an if-else conditional statement

---
+++ script.js
@@ -0,0 +1,13 @@
+// Write a conditional statement that outputs messages to drivers based on the traffic lights
+
+var light = "blue";
+
+if(light == "green") {
+    console.log("Drive")
+} else if(light == "orange") {
+    console.log("Get Ready")
+} else if(light == "red") {
+    console.log("Don't drive")
+} else {
+    console.log("Traffic light malfunction")
+}
